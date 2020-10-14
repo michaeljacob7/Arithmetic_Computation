fd701e0c6fbbd61466f431e810bff3afa9f65fe3
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-14 12:10:59 +0530

    Compute a+b*c added

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -5,3 +5,6 @@ echo "Welcome to the Arithmetic Computation and Sorting Program"
 read -p "Enter the first input: " input1
 read -p "Enter the second input: " input2
 read -p "Enter the third input: " input3
+
+result=$(($input1+$input2*$input3))
+echo "Result is: "$result
