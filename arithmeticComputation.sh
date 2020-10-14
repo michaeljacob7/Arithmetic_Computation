65309ffbcd948a71bae013568035e0547ac78ad9
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-14 11:49:29 +0530

    3 inputs taken

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -2,3 +2,6 @@
 
 echo "Welcome to the Arithmetic Computation and Sorting Program"
 
+read -p "Enter the first input: " input1
+read -p "Enter the second input: " input2
+read -p "Enter the third input: " input3
